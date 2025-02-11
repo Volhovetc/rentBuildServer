5da0fd6e2fc1af7bf50a6ddccfa11978910926a9
ArtemRodnaev <ihtiandr123.com@yandex.ru>
2025-02-11 14:31:44 +0500

    Сервер 0.0.5

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -27,21 +27,21 @@ router.post(
       }
       const pass = randomUUID();
       const hashedPassword = passwordHash.generate(pass);
-      const transporter = nodemailer.createTransport({
-        host: process.env.HOSTNAME,
-        port: 465,
-        secure: true,
-        auth: {
-          user: process.env.BOT,
-          pass: process.env.PASSWORD,
-        },
-      });
-      await transporter.sendMail({
-        from: process.env.BOT,
-        to: email,
-        subject: "Создание аккаунта",
-        text: `Ваш пароль для входа: ${pass}`,
-      });
+      // const transporter = nodemailer.createTransport({
+      //   host: process.env.HOSTNAME,
+      //   port: 465,
+      //   secure: true,
+      //   auth: {
+      //     user: process.env.BOT,
+      //     pass: process.env.PASSWORD,
+      //   },
+      // });
+      // await transporter.sendMail({
+      //   from: process.env.BOT,
+      //   to: email,
+      //   subject: "Создание аккаунта",
+      //   text: `Ваш пароль для входа: ${pass}`,
+      // });
 
       const user = new User({
         email,
