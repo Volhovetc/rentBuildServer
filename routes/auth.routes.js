5f3d0c41aca2c36771beda28343dc6251eddfcdf
ArtemRodnaev <ihtiandr123.com@yandex.ru>
2025-02-12 13:50:38 +0500

    Сервер 0.0.25

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -4,7 +4,6 @@ const User = require("../models/User");
 const { check, validationResult } = require("express-validator");
 const nodemailer = require("nodemailer");
 const router = Router();
-const { randomUUID } = require("crypto");
 const passwordHash = require("password-hash");
 const jwt = require("jsonwebtoken");
 
@@ -18,7 +17,7 @@ router.post(
       if (!error.isEmpty()) {
         return res.status(400).json({ type: "error", value: error.array() });
       }
-      const pass = randomUUID();
+      const pass = customPassword();
       const hashedPassword = passwordHash.generate(pass);
       const { email } = req.body;
       const candidate = await User.findOne({ email });
@@ -124,3 +123,45 @@ const sendMail = async (email, pass) => {
   //   text: `Ваш пароль для входа: ${pass}`,
   // });
 };
+
+const maxLength = 18;
+const minLength = 12;
+const uppercaseMinCount = 3;
+const lowercaseMinCount = 3;
+const numberMinCount = 2;
+const specialMinCount = 2;
+const UPPERCASE_RE = /([A-Z])/g;
+const LOWERCASE_RE = /([a-z])/g;
+const NUMBER_RE = /([\d])/g;
+const SPECIAL_CHAR_RE = /([\?\-])/g;
+const NON_REPEATING_CHAR_RE = /([\w\d\?\-])\1{2,}/g;
+
+function isStrongEnough(password) {
+  const uc = password.match(UPPERCASE_RE);
+  const lc = password.match(LOWERCASE_RE);
+  const n = password.match(NUMBER_RE);
+  const sc = password.match(SPECIAL_CHAR_RE);
+  const nr = password.match(NON_REPEATING_CHAR_RE);
+  return (
+    password.length >= minLength &&
+    !nr &&
+    uc &&
+    uc.length >= uppercaseMinCount &&
+    lc &&
+    lc.length >= lowercaseMinCount &&
+    n &&
+    n.length >= numberMinCount &&
+    sc &&
+    sc.length >= specialMinCount
+  );
+}
+
+function customPassword() {
+  let password = "";
+  const randomLength =
+    Math.floor(Math.random() * (maxLength - minLength)) + minLength;
+  while (!isStrongEnough(password)) {
+    password = generatePassword(randomLength, false, /[\w\d\?\-]/);
+  }
+  return password;
+}
